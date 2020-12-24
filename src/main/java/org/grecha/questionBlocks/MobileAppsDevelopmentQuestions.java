a645140d5f985e2b65d3af1441bcd30e88b29e99
avivanov <avivanov@naumen.ru>
2020-12-25 00:56:44 +0500

    Составил вопросы и ответы

--- src/main/java/org/grecha/questionBlocks/MobileAppsDevelopmentQuestions.java
+++ src/main/java/org/grecha/questionBlocks/MobileAppsDevelopmentQuestions.java
@@ -5,10 +5,26 @@ public class MobileAppsDevelopmentQuestions extends QuestionBlock {
     public MobileAppsDevelopmentQuestions() {
         this.setTitle("Разработка мобильных приложений");
         this.setQuestions(new Question[]{
-                new Question("1", new String[]{"A", "B", "C"}, 0),
-                new Question("2", new String[]{"D", "E", "F"}, 0),
-                new Question("3", new String[]{"G", "H", "I"}, 0),
-                new Question("4", new String[]{"J", "K", "L"}, 0)
+                new Question("Набор средств программирования, который содержит инструменты, необходимые для создания, компиляции и сборки мобильного Android приложения называется:", new String[]{
+                        "1.  Android SDK",
+                        "2. Android NDK",
+                        "3. JDK"},
+                        0),
+                new Question("Ядро какой операционной системы использовалось в качестве базы для ОС Android?", new String[]{
+                        "1. Windows",
+                        "2. Android OS",
+                        "3. Linux"},
+                        2),
+                new Question("В среде Intel XDK можно разрабатывать приложения для следующих платформ", new String[]{
+                        "1. Android",
+                        "2. Apple iOS",
+                        "3. Оба варианта верны"},
+                        2),
+                new Question("Возможен ли перенос приложений iOS в среду HTML5:", new String[]{
+                        "1. Нет, прямой перенос приложений невозможен",
+                        "2. Да, используя средства Intel XDK",
+                        "3. Да, только для iPhone, используя средства Intel XDK"},
+                        1)
         });
     }
 }
